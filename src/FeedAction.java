8773ea3a5315af3be8981e025d277bef369ec4fc
eallen <eric.holly.allen@gmail.com>
2016-09-22 12:35:37 -0600

    Feed the animals

---
+++ src/FeedAction.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+public class FeedAction {
+    private Date date;
+    private int quantity = 0;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     private static Map<SpeciesEnum, FoodTypesEnum> whatASpeciesEats = new HashMap<>();
     private static Map<String, SpeciesEnum> animals = new HashMap<>();
     private static Map<FoodTypesEnum, Integer> feedOnHand = new HashMap<>();
+    private static List<FeedAction> feedingRecord = new ArrayList<>();
+
+    private static int minimumFoodQuantity = 10;
 
     public static void main(String[] args) {
         whatASpeciesEats.put(SpeciesEnum.ELEPHANT, FoodTypesEnum.VEGGIES);
@@ -19,6 +21,10 @@ public class Main {
 
         newFeed(10, FoodTypesEnum.MEAT);
         newFeed(100, FoodTypesEnum.VEGGIES);
+
+        feedAnimal(50, new Date(), "Bob");
+        feedAnimal(10, new Date(), "Simba");
+        feedAnimal(20, new Date(), "Simba");
     }
 
 
@@ -30,5 +36,38 @@ public class Main {
             feedOnHand.put(type, quantity);
         }
         System.out.println("Type = " + type + " Quantity = " + feedOnHand.get(type));
+    }
+
+    public static void feedAnimal(int quantity, Date date, String name){
+        if(quantity < 0){
+            System.out.println("Sorry you can't unfeed an animal");
+        }
+        else if(animals.get(name) != null){
+            FeedAction feedAction = new FeedAction();
+            feedAction.setDate(date);
+            feedAction.setName(name);
+
+            FoodTypesEnum foodType = whatASpeciesEats.get(animals.get(name));
+
+            if(feedOnHand.get(foodType) <= minimumFoodQuantity){
+                //order more
+            }
+
+            if(feedOnHand.get(foodType) < quantity){
+                System.out.println("Sorry you couldn't have feed them that much!!");
+                feedAction.setQuantity(feedOnHand.get(foodType));
+                feedOnHand.put(foodType, 0);
+            }
+            else{
+                feedAction.setQuantity(quantity);
+                feedOnHand.put(foodType, feedOnHand.get(foodType) - quantity);
+            }
+
+            feedingRecord.add(feedAction);
+            System.out.println("The amount of " + foodType + " you have left " + feedOnHand.get(foodType));
+        }
+        else{
+            System.out.println("Sorry that animal is an imposter catch it!!");
+        }
     }
 }
